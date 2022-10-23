e7fb0a14e47fee350eee9227498c02cfe208aaf9
Kim Persson <kimpersson88@gmail.com>
2022-10-23 16:57:59 +0800

    fix: only depend on @ethersproject/address & separate dev dependencies

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,9 +1,9 @@
-import { ethers } from 'ethers';
+import { getAddress} from '@ethersproject/address'
 import data from './map.json';
 
 export const image = (address: string) => {
   try {
-    const addy = ethers.utils.getAddress(address);
+    const addy = getAddress(address);
     const found = data.find((x: any) => x.id === addy || x.id === address.toLowerCase());
     if (!found) return;
     return `https://raw.githubusercontent.com/albertocevallos/token-icons/main/assets/blockchains/${found.chain}/assets/${found.id}/logo.png`;
